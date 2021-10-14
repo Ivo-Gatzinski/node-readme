1b9790a4583550b83954298d49c1d0a349b2ff01
Ezhko <ucsd.camp.21@gmail.com>
2021-10-13 20:39:28 -0700

    add condition for license section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -47,8 +47,12 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
+  if (link) {
   return `## License
   This app's license is ${renderLicenseBadge(license)}. Please visit ${renderLicenseLink(license)} for more information.`
+  } else {
+    return "";
+  }
 }
 
 // TODO: Create a function to generate markdown for README
