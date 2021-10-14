5eb405c0ed010eb3cd7913543bb61d9e0fc12e3f
Ezhko <ucsd.camp.21@gmail.com>
2021-10-13 17:46:47 -0700

    add readme template and modules

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 // TODO: Create an array of questions for user input
 const questions = [];
 
